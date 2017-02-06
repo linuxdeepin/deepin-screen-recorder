af1b08aafb7e0c8aa6ae3d2161821a3e095af548
Andy Stewart <lazycat.manatee@gmail.com>
2017-02-06 22:08:20 +0800

    Add duration argument to byzanz-record, default is 864000 (10 days).

--- record_process.cpp
+++ record_process.cpp
@@ -30,6 +30,7 @@ void RecordProcess::run()
     file.remove();
 
     QStringList arguments;
+    arguments << QString("--duration=%1").arg(864000);
     arguments << QString("--x=%1").arg(recordX) << QString("--y=%1").arg(recordY);
     arguments << QString("--width=%1").arg(recordWidth) << QString("--height=%1").arg(recordHeight);
     arguments << savepath;
