0de810b5abb3f29d971955b378ffc6587a696e6c
Andy Stewart <lazycat.manatee@gmail.com>
2017-02-08 20:15:56 +0800

    Adjust app icon.

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     MainWindow window;
 
     window.setWindowTitle("Deepin recorder");
-    window.setWindowIcon(QIcon("logo.png"));
+    window.setWindowIcon(QIcon("image/deepin-recorder.svg"));
     window.setWindowFlags(Qt::Widget | Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
     window.setAttribute(Qt::WA_NoSystemBackground, true);
     window.setAttribute(Qt::WA_TranslucentBackground, true);
