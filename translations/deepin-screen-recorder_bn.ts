2856293cc6576155655efe7600ef04085e12bed4
transifex <transifex@linuxdeepin.com>
2018-01-25 08:01:14 +0000

    auto sync po files from transifex
    Change-Id: I58dcfef4a92b41a92812cebb4dc66a77c099d155

---
+++ translations/deepin-screen-recorder_bn.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="bn" version="2.1">
+<context>
+    <name>CountdownTooltip</name>
+    <message>
+        <location filename="../src/countdown_tooltip.cpp" line="73"/>
+        <source>Click tray icon 
+or press the shortcut again to stop recording</source>
+        <translation>ট্রে আইকনে ক্লিক করুন 
+বা রেকর্ডিং বন্ধ করতে আবার শর্টকাট টিপুন</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/main_window.cpp" line="67"/>
+        <source>Deepin screen recorder</source>
+        <translation>ডিপিন স্ক্রিন রেকর্ডার</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.cpp" line="118"/>
+        <source>Start recording</source>
+        <translation>রেকর্ডিং শুরু করুন</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.cpp" line="387"/>
+        <source>Select area</source>
+        <translation>জায়গা নির্বাচন করুন</translation>
+    </message>
+</context>
+<context>
+    <name>RecordProcess</name>
+    <message>
+        <location filename="../src/record_process.cpp" line="137"/>
+        <source>deepin-screen-recorder</source>
+        <translation>ডিপিন-স্ক্রিন-রেকর্ডার</translation>
+    </message>
+    <message>
+        <location filename="../src/record_process.cpp" line="185"/>
+        <source>View</source>
+        <translation>দেখুন</translation>
+    </message>
+    <message>
+        <location filename="../src/record_process.cpp" line="195"/>
+        <source>Record finished</source>
+        <translation>রেকর্ড শেষ</translation>
+    </message>
+    <message>
+        <location filename="../src/record_process.cpp" line="196"/>
+        <source>Saved to</source>
+        <translation>এখানে সংরক্ষণ করুন</translation>
+    </message>
+</context>
+<context>
+    <name>StartTooltip</name>
+    <message>
+        <location filename="../src/start_tooltip.cpp" line="45"/>
+        <source>Click or drag to
+select the area to record</source>
+        <translation>রেকর্ড করতে জায়গা নির্বাচন করতে
+ক্লিক করুন অথবা টেনে আনুন</translation>
+    </message>
+</context>
+<context>
+    <name>WindowManager</name>
+    <message>
+        <location filename="../src/window_manager.cpp" line="150"/>
+        <location filename="../src/window_manager.cpp" line="169"/>
+        <source>Desktop</source>
+        <translation>ডেস্কটপ</translation>
+    </message>
+</context>
+</TS>
