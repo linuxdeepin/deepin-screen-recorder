7af014376c102048863db39a0c9fbae9ef94073a
Huang Wei <wei.huang@deepin.io>
2019-09-29 11:15:19 +0800

    fix:fix screenshot shortcut timer bug

--- src/main.cpp
+++ src/main.cpp
@@ -248,6 +248,15 @@ int main(int argc, char *argv[])
 
 
 
+            QDBusConnection conn = QDBusConnection::sessionBus();
+            if (!conn.registerService("com.deepin.Screenshot") ||
+                    !conn.registerObject("/com/deepin/Screenshot", &window)) {
+                qDebug() << "deepin-screenshot is running!";
+
+                qApp->quit();
+                return 0;
+            }
+
             return app.exec();
         }
     } else {
