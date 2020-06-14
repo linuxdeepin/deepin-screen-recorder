2588fe118d277b9d2e49c0cda7c0b97790832cee
ut000593 <hemingyang@uniontech.com>
2020-06-14 11:22:15 +0800

    Title:fix, 龙芯上出现，工具栏图标位置错误问题。
    Description:bug number:32713
    
    RootCause:Null
    
    Solution:

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -1336,17 +1336,14 @@ void MainWindow::updateToolBarPos()
         m_toolBar->setRecordLaunchMode(m_launchWithRecordFunc);
         m_toolBar->setIsZhaoxinPlatform(m_isZhaoxin);
 
-        QString arch = QSysInfo::currentCpuArchitecture();
-        if(!arch.startsWith("MIPS", Qt::CaseInsensitive)) {
-            m_pVoiceVolumeWatcher = new voiceVolumeWatcher(this);
-            m_pVoiceVolumeWatcher->start();
-            connect(m_pVoiceVolumeWatcher, SIGNAL(sigRecodeState(bool)), this, SLOT(on_CheckRecodeCouldUse(bool)));
-            m_toolBarInit = true;
+        m_pVoiceVolumeWatcher = new voiceVolumeWatcher(this);
+        m_pVoiceVolumeWatcher->start();
+        connect(m_pVoiceVolumeWatcher, SIGNAL(sigRecodeState(bool)), this, SLOT(on_CheckRecodeCouldUse(bool)));
+        m_toolBarInit = true;
 
-            m_pCameraWatcher = new CameraWatcher(this);
-            m_pCameraWatcher->start();
-            connect(m_pCameraWatcher, SIGNAL(sigCameraState(bool)), this, SLOT(on_CheckVideoCouldUse(bool)));
-        }
+        m_pCameraWatcher = new CameraWatcher(this);
+        m_pCameraWatcher->start();
+        connect(m_pCameraWatcher, SIGNAL(sigCameraState(bool)), this, SLOT(on_CheckVideoCouldUse(bool)));
     }
 
     QPoint toolbarPoint;
