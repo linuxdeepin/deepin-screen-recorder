ad36ab02eb841494a15f74a157721aa3b2962f84
ut000593 <hemingyang@uniontech.com>
2020-06-22 15:55:22 +0800

    Title:fix kelvinU下，快捷键F11全屏截图失败。
    Description:task number:27033 , 原因，wayland平台上第二次调用Dbus接口org.kde.kwin.Screenshot，screenshotFullscreen方法返回pixmap的size为0x0。
    		在快捷键截取全屏的情况下，第二此调用Dbus接口和第一次获取的值完全一样。优化掉第二次调用，缩短截图时间(dbus 截图耗时严重)。
    
    RootCause:Null
    
    Solution:

--- src/utils/voicevolumewatcher.cpp
+++ src/utils/voicevolumewatcher.cpp
@@ -55,7 +55,7 @@ void voiceVolumeWatcher::run()
         */
          if (!m_isRecoding && m_loopwatch) {
              AudioPort activePort = m_defaultSource->activePort();
-             qDebug() << "=========" << activePort.name << activePort.description << activePort.availability << "--------";
+             //qDebug() << "=========" << activePort.name << activePort.description << activePort.availability << "--------";
              bool couldUse = false;
              if (isMicrophoneAvail(activePort.name)) {
                  double currentMicrophoneVolume = m_defaultSource->volume();
@@ -107,8 +107,6 @@ void voiceVolumeWatcher::initDeviceWatcher()
 
 void voiceVolumeWatcher::onCardsChanged(const QString &value)
 {
-    qDebug() << "Cards changed:" << value;
-
     initAvailInputPorts(value);
 }
 
@@ -141,7 +139,7 @@ void voiceVolumeWatcher::initAvailInputPorts(const QString &cards)
                 // 只添加输入port
                 if (port.isInputPort()) {
                     m_availableInputPorts.insert(port.portId, port);
-                    qDebug() << " " << port;
+                    //qDebug() << " " << port;
                 }
             }
         }
