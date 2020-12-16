15aeca5b4ff25cf3f6fa3f688cc0d5439d664dd6
ut000593 <hemingyang@uniontech.com>
2020-12-16 20:21:54 +0800

    Title:chore: CI执行脚本添加cppcheck命令, 输出检测报告.
    Description:
    
    RootCause:Null
    
    Solution:
    Change-Id: Ib7a3ee64d9d5705e20f08cecdbe5e9395f14aae9

--- tests/test-prj-running.sh
+++ tests/test-prj-running.sh
@@ -5,6 +5,9 @@ mkdir ../build-ut
 mkdir ../build-ut/report
 mkdir ../build-ut/html
 
+cppcheck --enable=all --output-file=../build-ut/cppcheck_deepin-screen-recorder.xml --xml  ../src/
+cppcheck --enable=all --output-file=../build-ut/cppcheck_dde-dock-plugins.xml --xml  ../dde-dock-plugins/
+
 ./ut_dde_dock_plugins/build_run_ut.sh
 ./ut_screen_shot_recorder/build_run_ut.sh
 
