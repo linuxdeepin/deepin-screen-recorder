5cf26a2eba4d52b966c821a4db58d2dbc160033c
wangcong <wangcong@uniontech.com>
2021-10-28 18:59:10 +0800

    fix:  修复cppcheck检查的错误
    Description:  修复静态检查时发现的一个cppcheck错误
    
    Log: no
    Change-Id: Iecd1a290ec4d81b4bac693c57378c86b1cdbf0b9

--- src/waylandrecord/waylandintegration.cpp
+++ src/waylandrecord/waylandintegration.cpp
@@ -91,7 +91,7 @@ QMap<quint32, WaylandIntegration::WaylandOutput> WaylandIntegration::screens()
     return globalWaylandIntegration->screens();
 }
 
-WaylandIntegration::WaylandIntegration * WaylandIntegration::waylandIntegration()
+WaylandIntegration::WaylandIntegration *WaylandIntegration::waylandIntegration()
 {
     return globalWaylandIntegration;
 }
@@ -102,7 +102,8 @@ void WaylandIntegration::WaylandOutput::setOutputType(const QString &type)
     const auto embedded = { QLatin1String("LVDS"),
                             QLatin1String("IDP"),
                             QLatin1String("EDP"),
-                            QLatin1String("LCD") };
+                            QLatin1String("LCD")
+                          };
 
     for (const QLatin1String &pre : embedded) {
         if (type.toUpper().startsWith(pre)) {
@@ -178,19 +179,19 @@ WaylandIntegration::WaylandIntegrationPrivate::WaylandIntegrationPrivate()
 WaylandIntegration::WaylandIntegrationPrivate::~WaylandIntegrationPrivate()
 {
     QMutexLocker locker(&m_mutex);
-    for (int i=0;i<m_freeList.size();i++){
+    for (int i = 0; i < m_freeList.size(); i++) {
         delete m_freeList[i];
     }
     m_freeList.clear();
-    for (int i=0;i<m_waylandList.size();i++){
+    for (int i = 0; i < m_waylandList.size(); i++) {
         delete m_waylandList[i]._frame;
     }
     m_waylandList.clear();
-    if(nullptr != m_ffmFrame){
+    if (nullptr != m_ffmFrame) {
         delete m_ffmFrame;
         m_ffmFrame = nullptr;
     }
-    if(nullptr != m_recordAdmin){
+    if (nullptr != m_recordAdmin) {
         delete m_recordAdmin;
         m_recordAdmin = nullptr;
     }
@@ -234,7 +235,7 @@ QMap<quint32, WaylandIntegration::WaylandOutput> WaylandIntegration::WaylandInte
 
 void WaylandIntegration::WaylandIntegrationPrivate::initWayland(QStringList list)
 {
-    m_recordAdmin = new RecordAdmin(list,this);
+    m_recordAdmin = new RecordAdmin(list, this);
     //设置获取视频帧
     setBGetFrame(true);
 
@@ -243,7 +244,8 @@ void WaylandIntegration::WaylandIntegrationPrivate::initWayland(QStringList list
 
     connect(m_connection, &KWayland::Client::ConnectionThread::connected, this, &WaylandIntegrationPrivate::setupRegistry, Qt::QueuedConnection);
     connect(m_connection, &KWayland::Client::ConnectionThread::connectionDied, this, [this] {
-        if (m_queue) {
+        if (m_queue)
+        {
             delete m_queue;
             m_queue = nullptr;
         }
@@ -251,7 +253,8 @@ void WaylandIntegration::WaylandIntegrationPrivate::initWayland(QStringList list
         m_connection->deleteLater();
         m_connection = nullptr;
 
-        if (m_thread) {
+        if (m_thread)
+        {
             m_thread->quit();
             if (!m_thread->wait(3000)) {
                 m_thread->terminate();
@@ -276,7 +279,7 @@ void WaylandIntegration::WaylandIntegrationPrivate::addOutput(quint32 name, quin
     KWayland::Client::Output *output = new KWayland::Client::Output(this);
     output->setup(m_registry->bindOutput(name, version));
 
-    connect(output, &KWayland::Client::Output::changed, this, [this, name, version, output] () {
+    connect(output, &KWayland::Client::Output::changed, this, [this, name, version, output]() {
         qCDebug(XdgDesktopPortalKdeWaylandIntegration) << "Adding output:";
         qCDebug(XdgDesktopPortalKdeWaylandIntegration) << "    manufacturer: " << output->manufacturer();
         qCDebug(XdgDesktopPortalKdeWaylandIntegration) << "    model: " << output->model();
@@ -297,7 +300,7 @@ void WaylandIntegration::WaylandIntegrationPrivate::addOutput(quint32 name, quin
                 return ;
             }
             m_remoteAccessManager = m_registry->createRemoteAccessManager(interface.name, interface.version);
-            connect(m_remoteAccessManager, &KWayland::Client::RemoteAccessManager::bufferReady, this, [this] (const void *output, const KWayland::Client::RemoteBuffer * rbuf) {
+            connect(m_remoteAccessManager, &KWayland::Client::RemoteAccessManager::bufferReady, this, [this](const void *output, const KWayland::Client::RemoteBuffer * rbuf) {
                 Q_UNUSED(output);
                 connect(rbuf, &KWayland::Client::RemoteBuffer::parametersObtained, this, [this, rbuf] {
                     processBuffer(rbuf);
@@ -320,7 +323,7 @@ void WaylandIntegration::WaylandIntegrationPrivate::removeOutput(quint32 name)
     qCDebug(XdgDesktopPortalKdeWaylandIntegration) << "    model: " << output.model();
 }
 
-void WaylandIntegration::WaylandIntegrationPrivate::processBuffer(const KWayland::Client::RemoteBuffer* rbuf)
+void WaylandIntegration::WaylandIntegrationPrivate::processBuffer(const KWayland::Client::RemoteBuffer *rbuf)
 {
     //qDebug() << Q_FUNC_INFO;
     QScopedPointer<const KWayland::Client::RemoteBuffer> guard(rbuf);
@@ -330,19 +333,17 @@ void WaylandIntegration::WaylandIntegrationPrivate::processBuffer(const KWayland
     quint32 stride = rbuf->stride();
     //    if(!bGetFrame())
     //        return;
-    if(m_bInitRecordAdmin)
-    {
+    if (m_bInitRecordAdmin) {
         m_bInitRecordAdmin = false;
-        m_recordAdmin->init(static_cast<int>(width),static_cast<int>(height));
+        m_recordAdmin->init(static_cast<int>(width), static_cast<int>(height));
         frameStartTime = av_gettime();
     }
     unsigned char *mapData = static_cast<unsigned char *>(mmap(nullptr, stride * height, PROT_READ, MAP_SHARED, dma_fd, 0));
-    if (MAP_FAILED == mapData)
-    {
-        qCWarning(XdgDesktopPortalKdeWaylandIntegration) << "dma fd " << dma_fd <<" mmap failed - ";
+    if (MAP_FAILED == mapData) {
+        qCWarning(XdgDesktopPortalKdeWaylandIntegration) << "dma fd " << dma_fd << " mmap failed - ";
     }
-    appendBuffer(mapData,static_cast<int>(width),static_cast<int>(height),static_cast<int>(stride),av_gettime()-frameStartTime);
-    munmap(mapData,stride * height);
+    appendBuffer(mapData, static_cast<int>(width), static_cast<int>(height), static_cast<int>(stride), av_gettime() - frameStartTime);
+    munmap(mapData, stride * height);
     close(dma_fd);
 }
 
@@ -371,55 +372,52 @@ void WaylandIntegration::WaylandIntegrationPrivate::appendBuffer(unsigned char *
     if (!bGetFrame() || nullptr == frame || width <= 0 || height <= 0) {
         return;
     }
-  
-    int size = height*stride;
+
+    int size = height * stride;
     unsigned char *ch = nullptr;
-    if(m_bInit)
-    {
+    if (m_bInit) {
         m_bInit = false;
         m_width = width;
         m_height = height;
         m_stride = stride;
         m_ffmFrame = new unsigned char[static_cast<unsigned long>(size)];
-        for (int i=0;i< m_bufferSize;i++)
-        {
+        for (int i = 0; i < m_bufferSize; i++) {
             ch = new unsigned char[static_cast<unsigned long>(size)];
             m_freeList.append(ch);
             //qDebug() << "创建内存空间";
         }
     }
     QMutexLocker locker(&m_mutex);
-    if(m_waylandList.size() >= m_bufferSize){
+    if (m_waylandList.size() >= m_bufferSize) {
         //先进先出
         //取队首
         waylandFrame wFrame = m_waylandList.first();
-        memset(wFrame._frame,0,static_cast<size_t>(size));
+        memset(wFrame._frame, 0, static_cast<size_t>(size));
         //拷贝当前帧
-        memcpy(wFrame._frame,frame,static_cast<size_t>(size));
+        memcpy(wFrame._frame, frame, static_cast<size_t>(size));
         wFrame._time = time;
         wFrame._width = width;
         wFrame._height = height;
         wFrame._stride = stride;
+        wFrame._index = 0;
         //删队首
         m_waylandList.removeFirst();
         //存队尾
         m_waylandList.append(wFrame);
         //qDebug() << "环形缓冲区已满，删队首，存队尾";
-    }
-    else if(0 <= m_waylandList.size() &&  m_waylandList.size() < m_bufferSize)
-    {
-        if(m_freeList.size()>0)
-        {
+    } else if (0 <= m_waylandList.size() &&  m_waylandList.size() < m_bufferSize) {
+        if (m_freeList.size() > 0) {
             waylandFrame wFrame;
             wFrame._time = time;
             wFrame._width = width;
             wFrame._height = height;
             wFrame._stride = stride;
+            wFrame._index = 0;
             //分配空闲内存
             wFrame._frame = m_freeList.first();
-            memset(wFrame._frame,0,static_cast<size_t>(size));
+            memset(wFrame._frame, 0, static_cast<size_t>(size));
             //拷贝wayland推送的视频帧
-            memcpy(wFrame._frame,frame,static_cast<size_t>(size));
+            memcpy(wFrame._frame, frame, static_cast<size_t>(size));
             m_waylandList.append(wFrame);
             //qDebug() << "环形缓冲区未满，存队尾"
             //空闲内存占用，仅删除索引，不删除空间
@@ -439,7 +437,7 @@ bool WaylandIntegration::WaylandIntegrationPrivate::getFrame(waylandFrame &frame
         frame._frame = nullptr;
         return false;
     } else {
-        int size = m_height*m_stride;
+        int size = m_height * m_stride;
         //取队首，先进先出
         waylandFrame wFrame = m_waylandList.first();
         frame._width = wFrame._width;
@@ -450,7 +448,7 @@ bool WaylandIntegration::WaylandIntegrationPrivate::getFrame(waylandFrame &frame
         frame._frame = m_ffmFrame;
         frame._index = frameIndex++;
         //拷贝到 m_ffmFrame 视频帧缓存
-        memcpy(frame._frame,wFrame._frame,static_cast<size_t>(size));
+        memcpy(frame._frame, wFrame._frame, static_cast<size_t>(size));
         //删队首视频帧 waylandFrame，未删空闲内存 waylandFrame::_frame，只删索引，不删内存空间
         m_waylandList.removeFirst();
         //回收空闲内存，重复使用
