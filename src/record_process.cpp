faadf81c0753fc8a567fcf181dcf870be57dc6b7
fengli <fengli@uniontech.com>
2022-08-03 18:25:56 +0800

    fix: 打开频蔽的数据埋点
    打开频蔽的数据埋点
    
    Log: 打开频蔽的数据埋点

--- src/record_process.cpp
+++ src/record_process.cpp
@@ -634,12 +634,12 @@ void RecordProcess::startRecord()
         }
     }
 
-//    QJsonObject obj{
-//        {"tid", EventLogUtils::StartRecording},
-//        {"version", QCoreApplication::applicationVersion()},
-//        {"type", recordType == RECORD_TYPE_GIF ? "gif" : (recordType == RECORD_TYPE_MKV ? "mkv" : "mp4")}
-//    };
-//    EventLogUtils::get().writeLogs(obj);
+    QJsonObject obj{
+        {"tid", EventLogUtils::StartRecording},
+        {"version", QCoreApplication::applicationVersion()},
+        {"type", recordType == RECORD_TYPE_GIF ? "gif" : (recordType == RECORD_TYPE_MKV ? "mkv" : "mp4")}
+    };
+    EventLogUtils::get().writeLogs(obj);
 
     if (Utils::isSysHighVersion1040() == false) {
         return;
@@ -670,12 +670,12 @@ void RecordProcess::emitRecording()
 
 void RecordProcess::stopRecord()
 {
-//    QJsonObject obj{
-//        {"tid", EventLogUtils::EndRecording},
-//        {"version", QCoreApplication::applicationVersion()},
-//        {"type", recordType == RECORD_TYPE_GIF ? "gif" : (recordType == RECORD_TYPE_MKV ? "mkv" : "mp4")}
-//    };
-//    EventLogUtils::get().writeLogs(obj);
+    QJsonObject obj{
+        {"tid", EventLogUtils::EndRecording},
+        {"version", QCoreApplication::applicationVersion()},
+        {"type", recordType == RECORD_TYPE_GIF ? "gif" : (recordType == RECORD_TYPE_MKV ? "mkv" : "mp4")}
+    };
+    EventLogUtils::get().writeLogs(obj);
 
     if (Utils::isSysHighVersion1040() == true) {
         qDebug() << "Pause the screen recording timer!";
