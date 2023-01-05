0cb2ce2c256d8d0ee8f10d28fbf37434211cf99b
He MingYang <hemingyang@uniontech.com>
2023-01-05 17:33:25 +0800

    feat: Wayland 截图 (#282)
    Description: 保存到剪贴板延迟操作
    
    Log: 延迟操作卡死问题解决

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -1777,7 +1777,7 @@ void MainWindow::save2Clipboard(const QPixmap &pix)
                 if (curTime >= endTime) {
                     break;
                 }
-                QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
+                //QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
             }
             qInfo() << __FUNCTION__ << __LINE__ << "3s延时完成" << time(nullptr);
         }
