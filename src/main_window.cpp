b1846455febfe51a6bbb3f25ef7eac07399d72c6
wangcong <wangcong@uniontech.com>
2023-02-10 13:38:04 +0800

    fix: 修复录屏时提示段错误，导致无法录屏
    Description: 修复录屏时提示段错误，导致无法录屏
    
    Log: 修复录屏时提示段错误，导致无法录屏
    
    Bug: https://pms.uniontech.com/bug-view-185721.html

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -203,7 +203,7 @@ void MainWindow::initAttributes()
     //Qt::WindowStaysOnTopHint： 通知窗口系统该窗口应位于所有其他窗口之上。请注意，在 X11 上的某些窗口管理器上，您还必须传递 Qt::X11BypassWindowManagerHint 以使此标志正常工作。
     //Qt::X11BypassWindowManagerHint : 完全绕过窗口管理器。
     if (Utils::isWaylandMode) {
-        setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+        setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::WindowDoesNotAcceptFocus);
         if (this->windowHandle()) {
             this->windowHandle()->setProperty("_d_dwayland_window-type", "onScreenDisplay");
             qDebug() << "设置窗口属性 _d_dwayland_window-type: " << this->windowHandle()->property("_d_dwayland_window-type");
@@ -5574,11 +5574,11 @@ void MainWindow::startCountdown()
     if (!Utils::isWaylandMode) {
         hide();
         show();
-    } else {
+    }/* else {
         qDebug() << "wayland开始录屏之后不获取焦点";
         setWindowFlag(Qt::WindowDoesNotAcceptFocus);
         this->show();
-    }
+    }*/
     Utils::passInputEvent(static_cast<int>(this->winId()));
 
     repaint();
