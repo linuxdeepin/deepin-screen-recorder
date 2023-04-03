e883c0459728705da71675af2f15a56bd1498c4e
wangcong <wangcong@uniontech.com>
2023-04-03 16:24:23 +0800

    fix: 修复部分机型录屏结束后，播放视频有7秒没有声音
    Description: 修复部分机型录屏结束后，播放视频有7秒没有声音
    
    Log: 修复部分机型录屏结束后，播放视频有7秒没有声音

--- src/record_process.cpp
+++ src/record_process.cpp
@@ -265,7 +265,11 @@ void RecordProcess::recordVideo()
     if (recordAudioInputType == RECORD_AUDIO_INPUT_SYSTEMAUDIO || recordAudioInputType == RECORD_AUDIO_INPUT_MIC_SYSTEMAUDIO) {
         //lastAudioSink = audioUtils->currentAudioSink();
         arguments << QString("-thread_queue_size");
-        arguments << QString("32");
+        if ((arch.startsWith("ARM", Qt::CaseInsensitive))) {
+            arguments << QString("2048");
+        } else {
+            arguments << QString("32");
+        }
         arguments << QString("-fragment_size");
         arguments << QString("4096");
         arguments << QString("-f");
@@ -280,7 +284,11 @@ void RecordProcess::recordVideo()
     }
     if (recordAudioInputType == RECORD_AUDIO_INPUT_MIC || recordAudioInputType == RECORD_AUDIO_INPUT_MIC_SYSTEMAUDIO) {
         arguments << QString("-thread_queue_size");
-        arguments << QString("32");
+        if ((arch.startsWith("ARM", Qt::CaseInsensitive))) {
+            arguments << QString("2048");
+        } else {
+            arguments << QString("32");
+        }
         arguments << QString("-fragment_size");
         arguments << QString("4096");
         arguments << QString("-f");
