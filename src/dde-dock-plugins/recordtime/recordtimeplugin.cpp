9e69c347ea60454a9a8459fffeb4ffb6df60143c
zhanghongyuan <zhanghongyuan@uniontech.com>
2024-08-14 11:00:02 +0800

    fix: the status bar display error time
    fix the status bar display error time
    
    Log: fix the status bar display error time
    Bug: https://pms.uniontech.com/bug-view-262791.html

--- src/dde-dock-plugins/recordtime/recordtimeplugin.cpp
+++ src/dde-dock-plugins/recordtime/recordtimeplugin.cpp
@@ -65,6 +65,7 @@ QWidget *RecordTimePlugin::itemWidget(const QString &itemKey)
 
 void RecordTimePlugin::clear()
 {
+    m_timeWidget->setting()->setValue(RECORDER_TIME_STARTCONFIG, QTime(0,0,0));
     if (nullptr != m_timer)
     {
         m_timer->stop();
