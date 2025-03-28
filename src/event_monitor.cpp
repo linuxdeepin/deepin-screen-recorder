117de8ca92e4d0bfc08d2edc1db568d120d2b3f2
renbin <renbin@uniontech.com>
2025-03-28 15:41:02 +0800

    fix: restructure event monitor cleanup
    - Adjust cleanup timing to avoid race with record thread
    - Move cleanup logic to end of run method
    - Add 30s timeout to prevent deadlock
    
    Log: restructure event monitor cleanup.
    Bug: https://pms.uniontech.com/bug-view-306357.html

--- src/event_monitor.cpp
+++ src/event_monitor.cpp
@@ -32,17 +32,8 @@ void EventMonitor::releaseRes()
     if (m_display_datalink && m_display) {
         qInfo() << __FUNCTION__ << __LINE__ << "执行 XRecordDisableContext ...";
         XRecordDisableContext(m_display, m_context);
-        qInfo() << __FUNCTION__ << __LINE__ << "执行 XRecordFreeContext ...";
-        XRecordFreeContext(m_display, m_context);
-        qInfo() << __FUNCTION__ << __LINE__ << "执行 XSync ...";
-        XSync(m_display, True);
-        XSync(m_display_datalink, True);
-        qInfo() << __FUNCTION__ << __LINE__ << "执行 XCloseDisplay m_display_datalink...";
-        XCloseDisplay(m_display_datalink);
-        qInfo() << __FUNCTION__ << __LINE__ << "执行 XCloseDisplay m_display...";
-        XCloseDisplay(m_display);
-        m_display_datalink = nullptr;
-        m_display = nullptr;
+
+        XFlush(m_display);
     }
 }
 
@@ -87,6 +78,19 @@ void EventMonitor::run()
         fprintf(stderr, "XRecordEnableContext() failed\n");
         return;
     }
+    qInfo() << "XRecordEnableContext() finished.";
+
+    qInfo() << __FUNCTION__ << __LINE__ << "执行 XRecordFreeContext ...";
+    XRecordFreeContext(m_display, m_context);
+    qInfo() << __FUNCTION__ << __LINE__ << "执行 XSync ...";
+    XSync(m_display, true);
+    XSync(m_display_datalink, true);
+    qInfo() << __FUNCTION__ << __LINE__ << "执行 XCloseDisplay m_display_datalink...";
+    XCloseDisplay(m_display_datalink);
+    qInfo() << __FUNCTION__ << __LINE__ << "执行 XCloseDisplay m_display...";
+    XCloseDisplay(m_display);
+    m_display_datalink = nullptr;
+    m_display = nullptr;
 }
 
 void EventMonitor::callback(XPointer ptr, XRecordInterceptData *data)
